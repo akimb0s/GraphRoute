46b6f252b0fe3cc59856e089f95a0d613d01b707
akimb0s <166992619+akimb0s@users.noreply.github.com>
2026-01-13 08:52:21 +0300

    Update app.py
    for safety using debag=false

--- app.py
+++ app.py
@@ -75,5 +75,6 @@ def get_route():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
+
 
